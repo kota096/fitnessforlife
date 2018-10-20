e6a62d15e7d749afed1c64e3170d6b25a24e9ffd
kota096 <kurutaoku096@gmail.com>
2018-10-20 08:01:56 +0000

    色

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module SampleApp
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
