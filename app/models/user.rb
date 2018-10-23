94d4be8a676ef5cfb21716d1c6d35cf34d30ff35
kota096 <kurutaoku096@gmail.com>
2018-10-23 11:59:41 +0000

    完成前テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -97,7 +97,7 @@ class User < ApplicationRecord
     if search
       where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
     else
-      all #全て表示。User.は省略
+      User.all #全て表示。User.は省略
     end
   end
 
